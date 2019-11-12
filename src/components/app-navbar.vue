127ec73501cb18fa29277712d1c8d6f635657335
samajaloleru <samsonajaloleru@gmail.com>
2019-11-12 13:48:38 +0100

    first commit

---
+++ src/components/app-navbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary fixed-top">
+      <div class="container">
+        <div class="navbar-translate">
+          <a class="navbar-brand" href="" rel="tooltip" title="" data-placement="bottom" target="_blank">
+      			<img class="img " src="@/assets/img/logo.png" />
+          </a>
+          <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar bar1"></span>
+            <span class="navbar-toggler-bar bar2"></span>
+            <span class="navbar-toggler-bar bar3"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <router-link  class="nav-link f4" to="/"> Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link  class="nav-link f4" to="/about"> About Us</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link  class="nav-link f4" to="/success">Success Stories</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link  class="nav-link f4" to="/contact">Contact Us</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </nav>
+</template>
+
+<script>
+import {HTTP} from '@/common';
+import notify from "@/components/notify"
+import appnavbar from "@/components/app-navbar";
+import appfooter from "@/components/app-footer";
+  const submenu = [        
+        {to:'/',icon:'',title:'Home'},        
+        {to:'/about',icon:'',title:'About Us'},        
+        {to:'/success',icon:'',title:'Success Stories'},        
+        {to:'/contact',icon:'',title:'Contact Us'},        
+        ]
+
+export default {
+	components: {notify, appnavbar, appfooter},
+	data(){ return { formSignup:{Username:"x",Password:"x",Email:""}, notifications:[]}},
+	methods: {
+      signup() {
+		const app = this;
+		if (app.formSignup.Email !== "") {
+			app.formSignup.Username = app.formSignup.Email;
+			app.formSignup.Password = app.formSignup.Email;
+		}
+        HTTP.post('/api/signup', app.formSignup ,{withCredentials: true}).then(response => {
+          console.log(response)
+          app.notifications.push(response.data)
+		  setTimeout(checkRedirect(response.data),1500)
+          if (response.data.Code == 200) {
+			  app.formSignup.Username = "x";
+			  app.formSignup.Password = "x";
+			  app.formSignup.Email = "";
+		  }
+        }).catch(e => {
+          console.log(e)
+          this.error = e
+          // this.errors.push(e)
+        })
+      }
+    }
+}
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,276 @@
+<template>
+	<section class="">
+    <appnavbar></appnavbar>
+		<div class="page-header page-header-small">
+      <div class="page-header-image" data-parallax="true" :style="{backgroundImage:'url('+require('@/assets/img/happy.jpeg')+')'}">
+      </div>
+      <div class="content-center">
+        <div class="row">
+          <div class="col-md-10 ml-auto mr-auto">
+						<br/><br/><br/>
+            <h1 class="text-danger">Your Support Means A lot To Us</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+		<div class="container" style="margin-top:10px">
+			<div class="row">
+				<div class="col-md-9 ml-auto mr-auto text-black">
+					<p><span  class="font-weight-bold"> REDEMPTION NETWORK..</span> A second chance to ensure Relief, Freedom, and Accomplishment.</p>
+					<p>Welcome to Redemption Network.. You are just a few steps away from Freedom and Stability!
+						<br/>Whether it’s considered “good debt” or “bad debt,” the truth is, any debt can cause serious emotional effects. 
+						Studies show what many of us already know: Debt is about much more than money. Being in debt can lead to a number of other emotional 
+						and psychological issues.
+					</p>
+					<p> Debt has generally become a menace in the society causing a lot of harm to both the young and the old in diverse ways. 
+						Depression and severe anxiety, Anxious feelings can arise with an array of triggers, such as constant worry about money, 
+						experiencing immense feelings of being overwhelmed with no end in sight, and hopelessness. 
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="testimonials-2 pt-0">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div id="carouselExampleIndicators2" class="my-0 carousel slide">
+							<ol class="carousel-indicators">
+								<li data-target="#carouselExampleIndicators2" data-slide-to="0" class="active"></li>
+								<li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
+							</ol>
+							<div class="carousel-inner" role="listbox">
+								<h2 class="font-weight-bold text-center text-primary mt-3">Featured Campaign</h2>
+								<div class="carousel-item active justify-content-center">
+									<div class="card card-testimonial card-plain bg-danger">
+										<div class="card-avatar pt-4">
+											<a>
+												<img class="img img-raised rounded" src="@/assets/img/sick-boy.jpg" />
+											</a>
+										</div>
+										<div class="card-body">
+											<p class="">Arham’s childhood cannot be compromised just because he was born poor. Right now, he needs our kind attention and financial aid for his healthy and stable future. Donate today to help this little soul!
+											</p>
+											<h4 class="font-weight-bold">Donate Arham Ali for his Duhamel Surgery</h4>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item justify-content-center">
+									<div class="card card-testimonial card-plain bg-danger">
+										<div class="card-avatar pt-4">
+											<a>
+												<img class="img img-raised rounded" src="@/assets/img/sick-girl.jpg" />
+											</a>
+										</div>
+										<div class="card-body">
+											<p class="">Sufiyan and his family are truly needy people and need our help to recover from this crisis. Your donation for Sufiyan’s surgery can give him a permanent relief and enable him to start his school.
+											</p>
+											<h4 class="font-weight-bold">Donate Muhammad Sufiyan for his Hernia Repair</h4>
+										</div>
+									</div>
+								</div>
+								<div class="text-center mb-5" style="margin-top:-50px">
+							 		<button class="btn btn-success btn-round bg-default" type="button">DONATE NOW</button>
+								</div>
+
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+								<i class="now-ui-icons arrows-1_minimal-left"></i>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators2" role="button" data-slide="next">
+								<i class="now-ui-icons arrows-1_minimal-right"></i>
+							</a>
+
+						</div>
+					</div>
+				</div>
+				<hr />
+			</div>
+		</div>
+		<div class="container pb-4" style="margin-top:-50px">
+			<div class="row">
+				<div class="col-md-9 ml-auto mr-auto">
+					<p> Debt is rough on anyone — especially when it encroaches on your marriage, partnership, or family. 
+						A spouse or partner may resent the other as a way of coping with debt. It is absolutely one of the leading predictors of divorce in recent times. 
+						Large amounts of debt have severe effects on a household’s psychological well-being and it isn’t uncommon to look back with regret. 
+						<br/>Having substantial debt can also increase your stress level at work, many become unproductive at their jobs and 
+						that can even complicate the problems when they lose their jobs due to constant worry and lack of focus.
+					</p>
+					<p> The average person has $16,000 in debt. Meanwhile, the average college student will graduate with a whopping $40,000 in student loans — 
+						and those who pursued graduate or higher degrees, switched majors, or went back to school may owe significantly more. 
+						In fact, about one in five borrowers owes $50,000 or more in student loans (and 5.6% owe more than $100,000), according to a survey. 
+						This is the same in different parts of the globe.
+						<br/>Round it out with car loans, personal loans, medical debt, mortgages and other obligations; It’s safe to assume that a large number of people carry some kind of debt. 
+					</p>
+					<p> A whopping 64% of graduate students said their constant concern over debt interferes with their optimal functioning,
+						<br/>Stress caused by debt doesn’t just affect the way we work and our day-to-day activities. That stress can also strip away the benefits of events that would normally be positive. 
+						The stress that comes from debt can eliminate all happiness you get from spending money on yourself, family and other important things. 
+						<br/>High debt levels have also significantly contributed to reduced marriage rates among young adults in the present day.
+					</p>
+				</div>
+				<div class="row bg-primary text-danger">
+            <div class="col-md-8 ml-auto mr-auto">
+              <div class="media-area">
+                <h2 class="title text-center">Success Stories</h2>                                            
+                <div class="media mt-3">
+                  <a class="pull-left">
+                    <div class="avatar">
+                      <img class="media-object img-raised" src="@/assets/img/karen.jpeg" alt="..." />
+                    </div>
+                  </a>
+                  <div class="media-body">
+                    <p class="font-weight-bold text-danger">God bless Redemption Network, for your help to making me debt free.
+										</p>
+										<h4 class="media-heading pull-right"> Karen 
+											<small class="text-muted text-danger">&middot;- Scarsdale, New York </small>
+                    </h4>  
+                  </div>
+                </div>                
+                <div class="media mt-3">
+                  <a class="pull-left">
+                    <div class="avatar">
+                      <img class="media-object img-raised" src="@/assets/img/naomi.jpg" alt="..." />
+                    </div>
+                  </a>
+                  <div class="media-body">
+                    <p class="font-weight-bold text-danger">Thank you so much for all your help. We were about to give up. 
+											We are honest hard working people and never intended for all this to happen. Thanks again!
+										</p>
+										<h4 class="media-heading pull-right"> Naomi  
+											<small class="text-muted text-danger">&middot;- Los Angeles, California </small>
+                    </h4>  
+                  </div>
+                </div>                
+              </div>
+            </div>
+          </div>
+			</div>
+		</div>
+		<div class="section bg-light">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5">
+						<h2><span class="font-weight-light">Most Secure Payment</span><br/>Methods Ever!</h2>
+						<ul class="h5">
+							<li>Officially 501 (c)(3) Approved for non Profit organizations</li>
+							<li>100% Secure Payment Certificate</li>
+							<li>Easiest Payment Methods</li>
+							<li>24/7 Donor Support Services</li>
+						</ul>
+					</div>
+					<div class="col-md-5 ml-auto mr-auto">						
+						<img  class="img " src="@/assets/img/visa-and-mastercard-logo-26.png" />
+						<img class="pt-5 img " src="@/assets/img/moneygram.png" />						
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section bg-default text-danger">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 ml-auto mr-auto">
+						<h2 class="text-info">Our Blog</h2>
+            <div class="card card-plain card-blog">
+							<div class="row">
+								<div class="col-md-8">
+									<h5 class="card-title">                      	
+										Things You Need to Know About Premenstrual Dysphoric Disorder (PMDD)											
+									</h5>
+									<br/>
+									<h5 class="text-success">	8th March </h5>
+									<p class="">
+									Menstruation is a woman’s monthly cycle, wherein vaginal bleeding occurs due to the shedding of the uterus. 
+									It is a normal disposition of a woman’s body which happens due to the absence of pregnancy. 
+									As a woman, experiencing monthly blood discharge is already uncomfortable enough. 
+									</p>                    
+								</div>
+							</div>
+						</div>
+            <div class="card card-plain card-blog">
+							<div class="row">
+								<div class="col-md-8">
+									<h5 class="card-title">                      	
+										List of Top 16 Trust Organizations in Pakistan											
+									</h5>
+									<br/>
+									<h5 class="text-success">	8th March </h5>
+									<p class="">
+										Kausar Parveen is a 52 years old homemaker suffering from Gallbladder stone for 5 years. Due to her disease,
+									</p>                    
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 ml-auto mr-auto">						
+						<h2 class="text-info">Press Release</h2>
+            <div class="card card-plain card-blog">
+							<div class="row">
+								<div class="col-md-8">
+									<h5 class="card-title">                      	
+										All You Need to Know About Zigron Cricket Fiesta 4 2019											
+									</h5>
+									<br/>
+									<h5 class="text-success">	8th March </h5>
+									<p class="">
+										The 4th Cricket Fiesta organized by Transparent Hands and Zigron Pakistan (Pvt) Ltd. continues in full swing with matches being played in Lahore and Islamabad. 
+										As the cricket battle of IT companies has spilled out from the boardrooms onto the cricket field, 
+									</p>                    
+								</div>
+							</div>
+						</div>
+            <div class="card card-plain card-blog">
+							<div class="row">
+								<div class="col-md-8">
+									<h5 class="card-title">                      	
+										Annual Strategic Meeting with Qasim Mueen, CSO & Co-Founder Transparent Hands											
+									</h5>
+									<br/>
+									<h5 class="text-success">	8th March </h5>
+									<p class="">
+										Success of any organization, no matter what size or industry, depends on effective strategic planning. 
+										A right strategic plan can provide clear road map to accomplish organization’s specific goals and it also increases the productivity. 
+									</p>                    
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<appfooter></appfooter>
+	</section>
+</template>
+
+<script>
+import {HTTP} from '@/common';
+import notify from "@/components/notify"
+import appnavbar from "@/components/app-navbar";
+import appfooter from "@/components/app-footer";
+
+export default {
+	components: {notify, appnavbar, appfooter},
+	data(){ return { formSignup:{Username:"x",Password:"x",Email:""}, notifications:[]}},
+	methods: {
+      signup() {
+		const app = this;
+		if (app.formSignup.Email !== "") {
+			app.formSignup.Username = app.formSignup.Email;
+			app.formSignup.Password = app.formSignup.Email;
+		}
+        HTTP.post('/api/signup', app.formSignup ,{withCredentials: true}).then(response => {
+          console.log(response)
+          app.notifications.push(response.data)
+		  setTimeout(checkRedirect(response.data),1500)
+          if (response.data.Code == 200) {
+			  app.formSignup.Username = "x";
+			  app.formSignup.Password = "x";
+			  app.formSignup.Email = "";
+		  }
+        }).catch(e => {
+          console.log(e)
+          this.error = e
+          // this.errors.push(e)
+        })
+      }
+    }
+}
+</script>
